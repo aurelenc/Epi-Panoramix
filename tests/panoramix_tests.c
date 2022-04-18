7daba80eff8f9b3b4b52a444b00ee71bfcc3c921
aurelenc <aurele.nicolas@epitech.eu>
2022-04-18 19:10:37 +0200

    [+] Added unit_test for druid refill

--- tests/panoramix_tests.c
+++ tests/panoramix_tests.c
@@ -8,7 +8,24 @@
 #include <criterion/criterion.h>
 #include "../include/panoramix.h"
 
+void druid_refill(druid_t *druid);
+
 Test(github_action, test)
 {
     cr_assert(0 == 0);
+}
+
+Test(druid, refill)
+{
+    druid_t druid;
+    common_data_t common;
+    params_t params;
+
+    params.nb_refills = 1;
+    params.pot_size = 5;
+    druid.params = &params;
+    druid.common = &common;
+    druid_refill(&druid);
+    cr_assert(druid.common->nb_potions == 5);
+    cr_assert(druid.params->nb_refills == 0);
 }
